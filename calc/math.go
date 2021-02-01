bf297ec76e1fdd146d5c25d8f9a3e68a59be6521
Lucas Silveira <lugano1995@gmail.com>
2021-02-01 12:02:39 -0300

    feat: add numbers length validation

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
+import "errors"
+
 // Add returns sum of integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (int, error) {
 	sum := 0
+
+	if len(numbers) < 2 {
+		return sum, errors.New("provide more than 2 numbers")
+	}
 
 	for _, num := range numbers {
 		sum += num
 	}
 
-	return sum
+	return sum, nil
 }
